af6ded2214cf85f500efe1b605523a6fc139162a
TrieuVD <trieuvd@runsystem.net>
2024-06-07 21:33:04 +0700

    update config fix format json datetime

--- src/main/java/vn/id/vuductrieu/tlcn_be/CorsConfig.java
+++ src/main/java/vn/id/vuductrieu/tlcn_be/CorsConfig.java
@@ -1,19 +1,21 @@
 package vn.id.vuductrieu.tlcn_be;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
