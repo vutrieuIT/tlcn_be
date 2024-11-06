0f395b177c4e23d8ac49d9c0c84fa60e4a42afc4
TrieuVD <vutrieu2002@gmail.com>
2024-11-06 22:02:05 +0700

    add ProductMongoDto, RatingMongoDto

---
+++ src/main/java/vn/id/vuductrieu/tlcn_be/dto/mongodb/ProductMongoDto.java
@@ -0,0 +1,6 @@
+package vn.id.vuductrieu.tlcn_be.dto.mongodb;
+
+import vn.id.vuductrieu.tlcn_be.entity.mongodb.ProductCollection;
+
+public class ProductMongoDto extends ProductCollection {
+}
---
+++ src/main/java/vn/id/vuductrieu/tlcn_be/dto/mongodb/RatingMongoDto.java
@@ -0,0 +1,6 @@
+package vn.id.vuductrieu.tlcn_be.dto.mongodb;
+
+import vn.id.vuductrieu.tlcn_be.entity.mongodb.RatingCollection;
+
+public class RatingMongoDto extends RatingCollection {
+}
