5df1172f6c63328955d80e1f0c8b4cee18295aef
TrieuVD <vutrieu2002@gmail.com>
2024-11-06 22:03:01 +0700

    add RatingRepo.findByProductId

--- src/main/java/vn/id/vuductrieu/tlcn_be/repository/mongodb/RatingRepo.java
+++ src/main/java/vn/id/vuductrieu/tlcn_be/repository/mongodb/RatingRepo.java
@@ -3,5 +3,8 @@ package vn.id.vuductrieu.tlcn_be.repository.mongodb;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import vn.id.vuductrieu.tlcn_be.entity.mongodb.RatingCollection;
 
+import java.util.List;
+
 public interface RatingRepo extends MongoRepository<RatingCollection, String> {
+    List<RatingCollection> findByProductId(Integer id);
 }
