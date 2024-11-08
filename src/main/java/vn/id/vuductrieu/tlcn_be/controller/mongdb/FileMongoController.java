4df43e048750c69d8c18431b72b989a30b87a3e5
TrieuVD <vutrieu2002@gmail.com>
2024-11-08 22:32:03 +0700

    add FileMongoController, GhnMongoController service

---
+++ src/main/java/vn/id/vuductrieu/tlcn_be/controller/mongdb/FileMongoController.java
@@ -0,0 +1,27 @@
+package vn.id.vuductrieu.tlcn_be.controller.mongdb;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import vn.id.vuductrieu.tlcn_be.service.ImageService;
+
+@RestController
+@RequestMapping("/api/mongo/file")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class FileMongoController {
+
+    private final ImageService imageService;
+
+    @PostMapping()
+    public String uploadImage(@RequestPart("file") MultipartFile file){
+        try {
+            return imageService.saveImage(file, "test");
+        } catch (Exception e){
+            return "server";
+        }
+    }
+}
---
+++ src/main/java/vn/id/vuductrieu/tlcn_be/controller/mongdb/GhnMongoController.java
@@ -0,0 +1,32 @@
+package vn.id.vuductrieu.tlcn_be.controller.mongdb;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.id.vuductrieu.tlcn_be.utils.GhnUtil;
+
+@RestController
+@RequestMapping("/api/mongo/ghn")
+@RequiredArgsConstructor
+public class GhnMongoController {
+
+    private final GhnUtil ghnUtil;
+
+    @GetMapping("/province")
+    public ResponseEntity getProvince() {
+        return ghnUtil.getProvince();
+    }
+
+    @GetMapping("/district/{provinceId}")
+    public ResponseEntity getDistrict(@PathVariable Integer provinceId) {
+        return ghnUtil.getDistrict(provinceId);
+    }
+
+    @GetMapping("/ward/{districtId}")
+    public ResponseEntity getWard(@PathVariable Integer districtId) {
+        return ghnUtil.getWard(districtId);
+    }
+}
