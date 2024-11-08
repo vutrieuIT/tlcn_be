80a60f88522e7974ee9bae837691790ed8448936
TrieuVD <vutrieu2002@gmail.com>
2024-11-08 22:51:34 +0700

    add order service

--- src/main/java/vn/id/vuductrieu/tlcn_be/repository/mongodb/OrderRepo.java
+++ src/main/java/vn/id/vuductrieu/tlcn_be/repository/mongodb/OrderRepo.java
@@ -1,7 +1,17 @@
 package vn.id.vuductrieu.tlcn_be.repository.mongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import vn.id.vuductrieu.tlcn_be.entity.mongodb.OrderCollection;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderRepo extends MongoRepository<OrderCollection, String> {
+
+    @Query("{'user': ?0}")
+    List<OrderCollection> findAllByUserId(String userId);
+
+    @Query("{'_id': ?0, 'user': ?1}")
+    OrderCollection findByIdAndUserId(String id, String userId);
 }
